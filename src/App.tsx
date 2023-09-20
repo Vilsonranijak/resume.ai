bd73e5a0cf0b98c970a3994c29756cc28350d5bc
Vilson Ranijak <79864082+VilsonRanijakjr@users.noreply.github.com>
2023-09-19 22:19:07 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import { Button } from "./components/ui/button";
 export default function App() {
   return (
     <>
-      <h1>Oi Amor!</h1>
-      <Button onClick={() => alert("Casa comigo?")}>Tem Amo ❤️</Button>
+      <h1>resume.ai</h1>
     </>
   );
 }
